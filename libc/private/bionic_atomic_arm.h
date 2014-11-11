76303a5ffed827eb808f45494c6021f056a274b3
Marco Milanese <marcomilanese7@gmail.com>
2014-11-11 13:45:01 +0200

    bionic: improve tls/atomic support for armv6
    Change-Id: I41e0059d405998ea738ebcbf8cbb5716fee572d1

--- libc/private/bionic_atomic_arm.h
+++ libc/private/bionic_atomic_arm.h
@@ -16,13 +16,30 @@
 #ifndef BIONIC_ATOMIC_ARM_H
 #define BIONIC_ATOMIC_ARM_H
 
-__ATOMIC_INLINE__ void __bionic_memory_barrier() {
-#if defined(ANDROID_SMP) && ANDROID_SMP == 1
-  __asm__ __volatile__ ( "dmb ish" : : : "memory" );
+#if defined(__thumb__) && !defined(__thumb2__)
+# define __ATOMIC_SWITCH_TO_ARM \
+"adr r3, 5f\n" \
+"bx r3\n" \
+".align\n" \
+".arm\n" \
+"5:\n"
+/* note: the leading \n below is intentional */
+# define __ATOMIC_SWITCH_TO_THUMB \
+"\n" \
+"adr r3, 6f+1\n" \
+"bx r3\n" \
+".thumb\n" \
+"6:\n"
+# define __ATOMIC_CLOBBERS "r3", /* list of clobbered registers */
 #else
+# define __ATOMIC_SWITCH_TO_ARM /* nothing */
+# define __ATOMIC_SWITCH_TO_THUMB /* nothing */
+# define __ATOMIC_CLOBBERS /* nothing */
+#endif
+
+__ATOMIC_INLINE__ void __bionic_memory_barrier() {
   /* A simple compiler barrier. */
   __asm__ __volatile__ ( "" : : : "memory" );
-#endif
 }
 
 /* Compare-and-swap, without any explicit barriers. Note that this function
@@ -33,6 +50,7 @@ __ATOMIC_INLINE__ int __bionic_cmpxchg(int32_t old_value, int32_t new_value, vol
   int32_t prev, status;
   do {
     __asm__ __volatile__ (
+          __ATOMIC_SWITCH_TO_ARM
           "ldrex %0, [%3]\n"
           "mov %1, #0\n"
           "teq %0, %4\n"
@@ -40,9 +58,10 @@ __ATOMIC_INLINE__ int __bionic_cmpxchg(int32_t old_value, int32_t new_value, vol
           "it eq\n"
 #endif
           "strexeq %1, %5, [%3]"
+          __ATOMIC_SWITCH_TO_THUMB
           : "=&r" (prev), "=&r" (status), "+m"(*ptr)
           : "r" (ptr), "Ir" (old_value), "r" (new_value)
-          : "cc");
+          : __ATOMIC_CLOBBERS "cc");
   } while (__builtin_expect(status != 0, 0));
   return prev != old_value;
 }
